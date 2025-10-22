843bfa8b453d7f88ef69dfc4f9b3639571695b50
MarkLi28 <markli@andrew.cmu.edu>
2025-10-22 19:47:32 +0000

    fixed formatting errors in my-test-file.js

--- my-test-file.js
+++ my-test-file.js
@@ -1,9 +1,9 @@
-
+'use strict';
 // my-test-file.js
 function check_for_nan_error() {
-  // Deliberately cause a NaN value
-  var result = 10 / "a";
-  console.log("Result:", result); // Output will be "Result: NaN"
+	// Deliberately cause a NaN value
+	var result = 10 / 'a';
+	console.log('Result:', result); // Output will be "Result: NaN"
 }
 check_for_nan_error();
 
