289f29f1305d0d7f1e8d7c61ecd1c051e716e3f4
Snehal Paliwal <snehalp@andrew.cmu.edu>
2025-11-12 00:53:04 +0000

    More merging conflict resolving

--- src/translate/index.js
+++ src/translate/index.js
@@ -40,16 +40,3 @@ translatorApi.translate = async function (postData) {
 	}
 
 };
-=======
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
-};
-
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
-// };
->>>>>>> main
